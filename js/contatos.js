fcf1e18f31aaa14ddc536cebf2ba29df18c5db38
Willian Silva <willian.silva.1992@hotmail.com>
2023-09-25 19:25:40 -0300

    add javascript nos contatos

--- js/contatos.js
+++ js/contatos.js
@@ -1,10 +1,15 @@
 // Captura o nome do usuário por meio de um prompt
-var userName = prompt("Digite seu nome:");
+var nome = prompt("Digite seu nome:");
 
-// Captura a idade do usuário por meio de um prompt
-var userAge = prompt("Digite sua idade:");
+// Captura o e-mail do usuário por meio de um prompt
+var email = prompt("Digite o seu e-mail:");
+// Captura o telefone do usuário por meio de um prompt
+var telefone = prompt("Digite o seu telefone:");
+// Captura a mensagem  do usuário por meio de um prompt
+var mensagem = prompt("Digite a sua mensagem:");
 
 // Exibe os dados capturados por meio de console.log
-console.log("Nome: " + userName);
-console.log("Idade: " + userAge);
-
+console.log("nome: " + nome);
+console.log("email: " + email);
+console.log("telefone: " + telefone);
+console.log("mensagem: " + mensagem);
