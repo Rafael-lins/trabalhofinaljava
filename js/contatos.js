bb9f00be174bb0c7e4432c439022a0bed9b6e09e
Willian Silva <willian.silva.1992@hotmail.com>
2023-09-25 19:16:40 -0300

    troca de imagem do time

---
+++ js/contatos.js
@@ -0,0 +1,10 @@
+// Captura o nome do usuário por meio de um prompt
+var userName = prompt("Digite seu nome:");
+
+// Captura a idade do usuário por meio de um prompt
+var userAge = prompt("Digite sua idade:");
+
+// Exibe os dados capturados por meio de console.log
+console.log("Nome: " + userName);
+console.log("Idade: " + userAge);
+
