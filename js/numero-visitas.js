1fe865a59982de654b05796313e8938b15fecb97
Willian Silva <willian.silva.1992@hotmail.com>
2023-11-13 13:45:58 -0300

    Ajustes do projeto final

--- js/numero-visitas.js
+++ js/numero-visitas.js
@@ -1,32 +1,13 @@
-// Função para obter o número de visitas armazenado no localStorage
-function getVisitorCount() {
-    const count = parseInt(localStorage.getItem('visitorData')) || 0;
-    return count;
+
+// Função para obter os dados dos visitantes armazenados no localStorage
+function getVisitorData() {
+    return JSON.parse(localStorage.getItem('visitorData')) || { count: 0, records: {} };
 }
 
 // Função para exibir e atualizar o número de visitas
 function displayVisitorCount() {
-    const visitorCount = getVisitorCount();
-    console.log(`Número de visitantes: ${visitorCount}`);
-    
-    // Atualize o número de visitas no localStorage
-    localStorage.setItem('visitorData', JSON.stringify({ visitorCount: visitorCount + 1, formRecords: formRecords }));
-}
-
-// Inicializando com um objeto vazio para armazenar registros do formulário de contatos
-let formRecords = {};
-
-function displayPromptAndLog(inputId) {
-    const inputValue = document.getElementById(inputId).value;
-    if (inputValue) {
-        // Registrar o valor de entrada
-        console.log(`Input (${inputId}): ${inputValue}`);
-        
-        // Salvar o registro do formulário no objeto usando o operador spread
-        formRecords[inputId] = inputValue;
-    } else {
-        console.log(`Input (${inputId}) is empty.`);
-    }
+    const visitorData = getVisitorData();
+    console.log(`Número de visitantes: ${visitorData.count}`);
 }
 
 // Função para limpar campos do formulário usando o operador spread
@@ -37,34 +18,39 @@ function clearFormFields() {
     }
 }
 
-// Função para enviar o formulário
-function submitForm() {
-    // Chame displayPromptAndLog para cada campo de entrada
-    displayPromptAndLog('name');
-    displayPromptAndLog('email');
-    displayPromptAndLog('phone');
-    displayPromptAndLog('message');
-    
-    // Limpe os campos do formulário usando clearFormFields()
-    clearFormFields();
-    
-    // Exibir todos os registros do formulário
-    displayFormRecords();
-
-    // Atualize o número de visitas e os registros do formulário no localStorage
-    displayVisitorCount();
-
-    // Habilite o botão "SEND MESSAGE" novamente
-    document.getElementById('submitButton').disabled = false;
-}
-
 // Função para exibir todos os registros do formulário
 function displayFormRecords() {
+    const visitorData = getVisitorData();
     console.log("Form Records:");
-    for (const inputId in formRecords) {
-        console.log(`Input (${inputId}): ${formRecords[inputId]}`);
+    for (const key in visitorData.records) {
+        console.log(`Input (${key}): ${visitorData.records[key]}`);
     }
 }
 
-// Chame a função para exibir e atualizar o número de visitas quando a página é carregada
-displayVisitorCount();
+// Função para enviar o formulário
+function submitForm() {
+    // Obter dados do formulário
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    // Armazenar os registros do formulário
+    const visitorData = getVisitorData();
+    visitorData.records = { name, email, phone, message };
+    visitorData.count += 1;
+    localStorage.setItem('visitorData', JSON.stringify(visitorData));
+
+    // Exibir todos os registros do formulário
+    displayFormRecords();
+
+    // Exibir e atualizar o número de visitas
+    displayVisitorCount();
+
+    // Limpar campos do formulário
+    clearFormFields();
+
+    // Habilitar o botão "SEND MESSAGE" novamente
+    document.getElementById('submitButton').disabled = false;
+}
+
